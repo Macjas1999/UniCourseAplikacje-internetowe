5d3d1467f45f9411880349cbd076c779f5d8fc1a
filio220 <161238756+filio220@users.noreply.github.com>
2024-06-22 21:06:01 +0200

    Update logged.php

--- app/views/logged.php
+++ app/views/logged.php
@@ -1,5 +1,12 @@
-﻿<body>
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rejestr Pracowników</title>
+    <link rel="stylesheet" href="app/views/CSS/styles.css">
+</head>
+<body>
     <nav class="navbar">
         <div class="logo">Rejestr Pracowników</div>
         <ul class="nav-links">
