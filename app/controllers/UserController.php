81acdd06bf720e560e294b82c46cf9317701180f
Drakkonnen <76957426+Drakkonnen@users.noreply.github.com>
2024-06-22 22:40:10 +0200

    cleanup

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -1,20 +1,23 @@
 <?php
 require_once BASE_PATH . '/app/models/EmployeeModel.php';
+
 class UserController
 {
     private EmployeeModel $employeeModel;
+
     public function __construct()
     {
-        $this->employeeModel = new EmployeeModel(require_once BASE_PATH . '/app/config/database.php');   
+        $this->employeeModel = new EmployeeModel(require_once BASE_PATH . '/app/config/database.php');
     }
 
-    public function handleRequest($user_type) : void {
-        console_log('userController');
+    public function handleRequest($user_type): void
+    {
         $page = $_GET['page'] ?? 'home';
-        console_log($_GET);
-        if ($page == 'logout'){
+
+        if ($page == 'logout') {
             $this->logout();
         }
+
         if ($user_type == 'user') {
             $this->handleUserRequest($page);
         } elseif ($user_type == 'admin') {
@@ -22,7 +25,8 @@ class UserController
         }
     }
 
-    private function logout() {
+    private function logout(): void
+    {
         session_unset();
         session_destroy();
         session_start();
@@ -35,7 +39,7 @@ class UserController
     {
         switch ($page) {
             case 'idk':
-                //
+                // Handle user specific request
                 break;
             default:
                 require_once BASE_PATH . '/app/views/logged.php';
@@ -45,7 +49,6 @@ class UserController
 
     private function handleAdminRequest($page): void
     {
-        console_log($_SERVER['REQUEST_METHOD'] . ' ' . $_SERVER['REQUEST_URI'] . ' ' . $page);
         switch ($page) {
             case 'employee_list':
                 $this->listEmployees();
@@ -57,11 +60,9 @@ class UserController
                 $this->updateEmployee();
                 break;
             case 'employee_remove':
-//                console_log($_SERVER['REQUEST_METHOD'] . ' ' . $_SERVER['REQUEST_URI'] . ' ' . $page);
                 $this->removeEmployee();
                 break;
             default:
-                console_log('require logged.php');
                 require_once BASE_PATH . '/app/views/logged.php';
                 break;
         }
@@ -83,53 +84,16 @@ class UserController
             } else {
                 require_once BASE_PATH . '/app/views/logged.php';
             }
-        }
-        else if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            console_log($_POST);
-            foreach ($_POST as $key => $value) {
-                $data[$key] = htmlspecialchars($value);
-            }
-            $errors = [];
-
-            if (empty($data['permissions']) || ($data['permissions'] != 'user' && $data['permissions'] != 'admin')) {
-                $errors['permissions'] = 'Permissions allowed are user and admin';
-            }
-
-            if (empty($data['first_name'])) {
-                $errors['first_name'] = 'First name is required';
-            }
-            if (empty($data['last_name'])) {
-                $errors['last_name'] = 'Last name is required';
-            }
-            if (empty($data['birth_date'])) {
-                $errors['birth_date'] = 'Birth date is required';
-            }
-            if (empty($data['address'])) {
-                $errors['address'] = 'Address is required';
-            }
-            if (empty($data['telephone'])) {
-                $errors['telephone'] = 'Telephone is required';
-            }
-            if (empty($data['job_position'])) {
-                $errors['job_position'] = 'Job position is required';
-            }
-            if (empty($data['date_started'])) {
-                $errors['date_started'] = 'Date started is required';
-            }
-            if (empty($data['salary'])) {
-                $errors['salary'] = 'Salary is required';
-            }
-            if (empty($data['email']) || !filter_var($data['email'], FILTER_VALIDATE_EMAIL)) {
-                $errors['email'] = 'Valid email is required';
-            }
+        } elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $data = $this->sanitizeInput($_POST);
+            $errors = $this->validateEmployeeData($data);
 
             if (empty($errors)) {
                 $this->employeeModel->updateEmployee($data);
-            }
-            else {
+            } else {
                 print_r($errors);
             }
-            console_log('update post');
+
             header('Location: index.php?page=employee_list');
         }
     }
@@ -138,64 +102,57 @@ class UserController
     {
         if ($_SERVER['REQUEST_METHOD'] === 'GET') {
             require_once BASE_PATH . '/app/views/Employee/add.php';
-        }
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            console_log($_POST);
-            foreach ($_POST as $key => $value) {
-                $data[$key] = htmlspecialchars($value);
-            }
-            $errors = [];
-
-            if (empty($data['permissions']) || ($data['permissions'] != 'user' && $data['permissions'] != 'admin')) {
-                $errors['permissions'] = 'Permissions allowed are user and admin';
-            }
-
-            if (empty($data['first_name'])) {
-                $errors['first_name'] = 'First name is required';
-            }
-            if (empty($data['last_name'])) {
-                $errors['last_name'] = 'Last name is required';
-            }
-            if (empty($data['birth_date'])) {
-                $errors['birth_date'] = 'Birth date is required';
-            }
-            if (empty($data['address'])) {
-                $errors['address'] = 'Address is required';
-            }
-            if (empty($data['telephone'])) {
-                $errors['telephone'] = 'Telephone is required';
-            }
-            if (empty($data['job_position'])) {
-                $errors['job_position'] = 'Job position is required';
-            }
-            if (empty($data['date_started'])) {
-                $errors['date_started'] = 'Date started is required';
-            }
-            if (empty($data['salary'])) {
-                $errors['salary'] = 'Salary is required';
-            }
-            if (empty($data['email']) || !filter_var($data['email'], FILTER_VALIDATE_EMAIL)) {
-                $errors['email'] = 'Valid email is required';
-            }
+        } elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $data = $this->sanitizeInput($_POST);
+            $errors = $this->validateEmployeeData($data);
 
             if (empty($errors)) {
                 $this->employeeModel->addEmployee($data);
                 require_once BASE_PATH . '/app/views/Employee/add.php';
+            } else {
+                foreach ($errors as $error) {
+                    echo $error . '<br>';
+                }
             }
-            else {
-                print_r($errors);
-            }
-//            header('Location: index.php?page=employee_list');
-        } else {
         }
     }
 
-    private function removeEmployee() {
-        
+    private function removeEmployee(): void
+    {
         $id = $_GET['id'] ?? null;
         if ($id && $id != $_SESSION['id']) {
             $this->employeeModel->removeEmployee($id);
         }
         header('Location: index.php?page=employee_list');
+    }
+
+    private function sanitizeInput(array $input): array
+    {
+        $sanitized = [];
+        foreach ($input as $key => $value) {
+            $sanitized[$key] = htmlspecialchars($value);
+        }
+        return $sanitized;
+    }
+
+    private function validateEmployeeData(array $data): array
+    {
+        $errors = [];
+
+        if (empty($data['permissions']) || ($data['permissions'] != 'user' && $data['permissions'] != 'admin')) {
+            $errors['permissions'] = 'Permissions allowed are user and admin';
+        }
+
+        foreach (['first_name', 'last_name', 'birth_date', 'address', 'telephone', 'job_position', 'date_started', 'salary', 'email'] as $field) {
+            if (empty($data[$field])) {
+                $errors[$field] = ucfirst(str_replace('_', ' ', $field)) . ' is required';
+            }
+        }
+
+        if (!filter_var($data['email'], FILTER_VALIDATE_EMAIL)) {
+            $errors['email'] = 'Valid email is required';
+        }
+
+        return $errors;
     }
 }
