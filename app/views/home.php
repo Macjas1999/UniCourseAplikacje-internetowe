bae99c2e724f8928fbe0618cad1da2e129d2f4f6
filio220 <filip.kluczniok@gmail.com>
2024-06-22 23:41:37 +0200

    koko jumbo i do przodu

--- app/views/home.php
+++ app/views/home.php
@@ -5,11 +5,13 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+ 
     <title>Document</title>
 </head>
 <body>
 
-<a href="<?php echo APP_NAME . "/?page=register"; ?>">Rejestracja</a>
-<a href="<?php echo APP_NAME . "/?page=login"; ?>">Login</a>
+
+
+
 </body>
 </html>
